84d05f6700b274c83f6b67c6393509bf33b966ed
tykhon_office <nameua@mail.ru>
2015-06-11 20:05:53 +0300

    2

--- src/test/java/com/auto/test/BaseTest.java
+++ src/test/java/com/auto/test/BaseTest.java
@@ -10,5 +10,4 @@ public class BaseTest {
     protected WebDriver driver;
 
 
-
 }
--- src/test/java/com/auto/test/TaskTest.java
+++ src/test/java/com/auto/test/TaskTest.java
@@ -1,37 +1,40 @@
 package com.auto.test;
 
+import java.net.MalformedURLException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.net.MalformedURLException;
-import com.auto.test.page.SearchPage;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Created by none on 09.06.15.
  */
-public class TaskTest extends BaseTest {
+public class TaskTest extends BaseTest{
+
+    public void startWebDriver()  {
+
+        System.setProperty("webdriver.chrome.driver", this.getClass().getClassLoader().getResource("chromedriver").getPath());
+
+    }
+
 
     @Before
     public void setUp() throws MalformedURLException {
-        //System.setProperty("webdriver.chrome.driver", "/usr/local/share/chromedriver");
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
-
+        //driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         driver.get("https://www.google.ru");
     }
 
     @After
     public void tearDown() {
         driver.close();
+        driver.quit();
     }
 
     @Test
     public void searchResult(){
-        SearchPage searchPage = new SearchPage(driver);
-        searchPage.fillSearchField("123");
+
 
     }
 
