e656e5badbece08dcfd5b32af08cd6e68d9b740a
tykhon_office <nameua@mail.ru>
2015-06-16 18:51:20 +0300

    java doc created and added

--- src/test/java/com/auto/test/BaseTest.java
+++ src/test/java/com/auto/test/BaseTest.java
@@ -1,13 +1,13 @@
 package com.auto.test;
 
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Represented all general methods for over the test cases.
  *
  * Created by none on 09.06.15.
  */
-
-import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
 
 public class BaseTest {
     private static Logger logger = Logger.getLogger(BaseTest.class);
